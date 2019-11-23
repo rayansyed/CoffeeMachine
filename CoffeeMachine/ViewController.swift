7ebae464014bf4fc595858d6975baa53fe2aeea7
Rayan Syed <rayansyed41@gmail.com>
2019-11-23 13:50:53 -0500

    Initial Commit

---
+++ CoffeeMachine/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoffeeMachine
+//
+//  Created by Rayan Syed on 2019-11-23.
+//  Copyright © 2019 Rayan Syed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
